04c177d00f61624d0b77e1157e3b18e2543f2b87
Adrian Khoo <adriankhoo.ca@gmail.com>
2018-12-21 12:15:25 -0800

    Added testing framework Jest

---
+++ db/tests/seed-helpers.test.js
@@ -0,0 +1,12 @@
+const seedHelpers = require('../seed-helpers')
+
+describe('randPrice', () => {
+  test('test1', () => {
+    expect(true).toEqual(true);
+  });
+});
+
+// hoursType
+// createRandomStock
+// generateTicker
+// createRandomCompany
