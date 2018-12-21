c67ed8a3e9d15705afac2aff1373b8b68bf02d8b
Adrian Khoo <adriankhoo.ca@gmail.com>
2018-12-21 14:15:13 -0800

    Added unit tests for seed helper functions

--- db/tests/seed-helpers.test.js
+++ db/tests/seed-helpers.test.js
@@ -3,16 +3,27 @@ const seedHelpers = require('../seed-helpers')
 
 describe('randPrice', () => {
 
-  xtest('Creates an object with a random price if a positive refPrice is provided and the maxChangeFactor is >0', () => {
-    expect(false).toEqual(true);
+  test('Creates an object with a random price if a positive refPrice is provided and the maxChangeFactor is >0', () => {
+    var price = seedHelpers.randPrice(undefined, new Date, 10, 0.5);
+    expect(price.price).not.toEqual(10);
   });
 
-  xtest('Creates an object with the correct shape if a priceType is provided', () => {
-    expect(false).toEqual(true);
+  test('Creates an object with the correct shape if a priceType is provided', () => {
+    var price = seedHelpers.randPrice("Pre-Market", new Date, 10, 0.5);
+    expect(price).toEqual(expect.objectContaining({
+      priceType: "Pre-Market",
+      datetime: expect.anything(),
+      price: expect.anything()
+    }));
   });
 
-  xtest('Creates an object with the correct shape if a priceType is NOT provided', () => {
-    expect(false).toEqual(true);
+  test('Creates an object with the correct shape if a priceType is NOT provided', () => {
+    var price = seedHelpers.randPrice(undefined, new Date, 10, 0.5);
+    expect(price).toEqual(expect.objectContaining({
+      datetime: expect.anything(),
+      price: expect.anything()
+    }));
+    expect(price.priceType).not.toBeDefined();
   });
 
 });
@@ -39,71 +50,90 @@ describe('hoursType', () => {
 
 describe('createRandomStock', () => {
 
-  xtest('Creates an object with the correct shape', () => {
-    expect(false).toEqual(true);
+  var date = new Date('2018-12-21T09:30:00');
+  var stock = seedHelpers.createRandomStock('RAND', 'Random Company Inc', 0.55, 350000, 120.25, date);
+
+  test('Creates an object with the correct shape', () => {
+    expect(stock).toEqual(expect.objectContaining({
+      ticker: 'RAND',
+      name: 'Random Company Inc',
+      analystBuy: 0.55,
+      platformOwners: 350000,
+      last1dPrices: expect.any(Object),
+      last1wPrices: expect.any(Object),
+      last1yPrices: expect.any(Object),
+      last5yPrices: expect.any(Object),
+    }));
   });
 
-  xtest('Latest price data point should be within business hours if the provided latestDateTime is outside business hours', () => {
-    expect(false).toEqual(true);
+  test('Latest price data point should be within business hours if the provided latestDateTime is outside business hours', () => {
+    expect(stock.last1dPrices[0].datetime).toEqual(date);
   });
 
-  xtest('last5y prices should contain a reasonable number of price objects', () => {
-    expect(false).toEqual(true);
+  test('last5y prices should contain a reasonable number of price objects', () => {
+    expect(stock.last5yPrices.length).toBeGreaterThan(200);
   });
 
-  xtest('last5y prices should span approximately 5 years', () => {
-    expect(false).toEqual(true);
+  test('last5y prices should span approximately 5 years', () => {
+    var lastDate = stock.last5yPrices.slice(-1)[0].datetime;
+    expect((date - lastDate) / (1000 * 60 * 60 * 24)).toBeGreaterThan(365 * 4.5);
   });
 
-  xtest('last1y prices should contain a reasonable number of price objects', () => {
-    expect(false).toEqual(true);
+  test('last1y prices should contain a reasonable number of price objects', () => {
+    expect(stock.last1yPrices.length).toBeGreaterThan(350);
   });
 
-  xtest('last1y prices should span approximately 5 years', () => {
-    expect(false).toEqual(true);
+  test('last1y prices should span approximately 1y', () => {
+    var lastDate = stock.last5yPrices.slice(-1)[0].datetime;
+    expect((date - lastDate) / (1000 * 60 * 60 * 24)).toBeGreaterThan(350);
   });
 
-  xtest('last1w prices should contain a reasonable number of price objects', () => {
-    expect(false).toEqual(true);
+  test('last1w prices should contain a reasonable number of price objects', () => {
+    expect(stock.last1wPrices.length).toBeGreaterThan(150);
   });
 
-  xtest('last1w prices should span approximately 5 years', () => {
-    expect(false).toEqual(true);
-  });
-
-  xtest('last1d prices should contain a reasonable number of price objects', () => {
-    expect(false).toEqual(true);
+  test('last1w prices should span approximately 1w', () => {
+    var lastDate = stock.last5yPrices.slice(-1)[0].datetime;
+    expect((date - lastDate) / (1000 * 60 * 60 * 24)).toBeGreaterThan(5);
   });
 
 });
 
 
 describe('generateTicker', () => {
-  xtest('Generates "ABOP" for "Apple Banana Orange Pineapple Cucumber"', () => {
-    expect(false).toEqual(true);
+  test('Generates "ABOP" for "Apple Banana Orange Pineapple Cucumber"', () => {
+    expect(seedHelpers.generateTicker('Apple Banana Orange Pineapple Cucumber')).toEqual('ABOP');
   });
 });
 
 
 describe('createRandomCompany', () => {
-  xtest('Creates a company with random data', () => {
-    expect(false).toEqual(true);
+  test('Creates a company with random data', () => {
+    expect(seedHelpers.createRandomCompany()).toEqual(expect.objectContaining({
+      name: expect.anything(),
+      ticker: expect.anything(),
+    }));
   });
 });
 
 
 describe('createRandomStocks', () => {
 
-  xtest('Output array should contain "num" numbers of stocks', () => {
-    expect(false).toEqual(true);
+  var date = new Date('2018-12-21T09:30:00');
+  var stocks = seedHelpers.createRandomStocks(20, date)
+
+  test('Output array should contain "num" numbers of stocks', () => {
+    expect(stocks.length).toEqual(20);
   });
 
-  xtest('Stocks should have lastestDateTime for the latest price', () => {
-    expect(false).toEqual(true);
+  test('Stocks should have lastestDateTime for the latest price', () => {
+    expect(stocks[0].last5yPrices[0].datetime).toEqual(date);
   });
 
-  xtest('No stock tickers should be repeated', () => {
-    expect(false).toEqual(true);
+  test('No stock tickers should be repeated', () => {
+    tickers = new Set;
+    stocks.forEach( stock => {tickers.add(stock.ticker)} );
+    expect(tickers.size).toEqual(20);
   });
 
 });
