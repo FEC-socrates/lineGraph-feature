55e5903f46440c6a045d0416ae31e885b07b679e
Adrian Khoo <adriankhoo.ca@gmail.com>
2018-12-21 11:32:50 -0800

    Modularized db config script for better separation of concerns. Added db seeding script to package.json.

---
+++ db/stock.js
@@ -0,0 +1,107 @@
+var mongoose = require('mongoose');
+var Schema = require('./config.js');
+
+
+// ============================================
+// ESTABLISHING SCHEMA
+// ============================================
+
+var stock = new mongoose.Schema({
+  ticker: String,
+  name: String,
+  analystBuy: Number,
+  platformOwners: Number,
+  last1dPrices: [
+    {
+      priceType: String,
+      datetime: Date,
+      price: Number
+    }
+  ],
+  last1wPrices: [
+    {
+      datetime: Date,
+      price: Number
+    }
+  ],
+  last1yPrices: [
+    {
+      datetime: Date,
+      price: Number
+    }
+  ],
+  last5yPrices: [
+    {
+      datetime: Date,
+      price: Number
+    }
+  ]
+});
+
+var Stock = mongoose.model('Stock', stock);
+
+
+// ============================================
+// DEFINING STOCK DB LOOKUP METHODS
+// ============================================
+
+Stock.listAll = () => {
+  // Returns a Promise object which resolves to a list of all stocks in the collection.
+  return Stock.find({}, 'ticker name');
+};
+
+Stock.getInfo = ticker => {
+  // Returns a Promise object which resolves to basic info for the stock matching the provided ticker.
+  return Stock.findOne(
+    {ticker: ticker}, 
+    'ticker name analystBuy platformOwners');
+};
+
+Stock.get1dPrices = ticker => {
+  // Returns a Promise object which resolves to 1d price history for the stock matching the provided ticker.
+  return Stock.findOne(
+    {ticker: ticker}, 
+    {'last1dPrices': 1});
+};
+
+Stock.get1wPrices = ticker => {
+  // Returns a Promise object which resolves to 1w price history for the stock matching the provided ticker.
+  return Stock.findOne(
+    {ticker: ticker}, 
+    {'last1wPrices': 1});
+};
+
+Stock.get1mPrices = ticker => {
+  // Returns a Promise object which resolves to 1m price history for the stock matching the provided ticker.
+  return Stock.aggregate([
+    {
+      $match: {ticker: ticker}
+    },
+    {
+      $project: {'last1yPrices': 1}
+    },
+    {
+      $unwind: '$last1yPrices'
+    },
+    {
+      $limit: 30
+    }
+  ]);
+};
+
+Stock.get1yPrices = ticker => {
+  // Returns a Promise object which resolves to 1w price history for the stock matching the provided ticker.
+  return Stock.findOne(
+    {ticker: ticker}, 
+    {'last1yPrices': 1});
+};
+
+Stock.get5yPrices = ticker => {
+  // Returns a Promise object which resolves to 1w price history for the stock matching the provided ticker.
+  return Stock.findOne(
+    {ticker: ticker}, 
+    {'last5yPrices': 1});
+};
+
+
+module.exports = Stock;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3456;
-var stocks = require('../db/config');
+var db = require('../db/config');
+var stocks = require('../db/stock');
+
+
+// ============================================
+// ESTABLISH API ENDPOINTS
+// ============================================
 
 // GET /stocks returns a list of all stocks
 app.get('/stocks/', (req, res) => {
@@ -11,14 +17,12 @@ app.get('/stocks/', (req, res) => {
 
 // GET /stocks/:ticker/ returns basic info about a stock
 app.get('/stocks/:ticker', (req, res) => {
-  console.log(req.method, req.path, req.body, req.params.ticker);
   stocks.getInfo(req.params.ticker)
     .then(results => { res.send(JSON.stringify(results)); });
 });
 
 // GET /stocks/:ticker/last1dPrices returns relevant price history for a stock
 app.get('/stocks/:ticker/last1dPrices', (req, res) => {
-  console.log(req.method, req.path, req.body, req.params.ticker);
   stocks.get1dPrices(req.params.ticker)
     .then(results => { res.send(JSON.stringify(results)); });
 });
@@ -26,30 +30,31 @@ app.get('/stocks/:ticker/last1dPrices', (req, res) => {
 
 // GET /stocks/:ticker/last1wPrices returns relevant price history for a stock
 app.get('/stocks/:ticker/last1wPrices', (req, res) => {
-  console.log(req.method, req.path, req.body, req.params.ticker);
   stocks.get1wPrices(req.params.ticker)
     .then(results => { res.send(JSON.stringify(results)); });
 });
 
 // GET /stocks/:ticker/last1mPrices returns relevant price history for a stock
 app.get('/stocks/:ticker/last1mPrices', (req, res) => {
-  console.log(req.method, req.path, req.body, req.params.ticker);
   stocks.get1mPrices(req.params.ticker)
     .then(results => { res.send(JSON.stringify(results)); });
 });
 
 // GET /stocks/:ticker/last1yPrices returns relevant price history for a stock
 app.get('/stocks/:ticker/last1yPrices', (req, res) => {
-  console.log(req.method, req.path, req.body, req.params.ticker);
   stocks.get1yPrices(req.params.ticker)
     .then(results => { res.send(JSON.stringify(results)); });
 });
 
 // GET /stocks/:ticker/last5yPrices returns relevant price history for a stock
 app.get('/stocks/:ticker/last5yPrices', (req, res) => {
-  console.log(req.method, req.path, req.body, req.params.ticker);
   stocks.get5yPrices(req.params.ticker)
     .then(results => { res.send(JSON.stringify(results)); });
 });
+
+
+// ============================================
+// START THE SERVER
+// ============================================
 
 app.listen(port, () => console.log('server is listening!'));
