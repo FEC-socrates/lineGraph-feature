90d0d4cea2df2ea3a065c9196383c1cf412ed392
Adrian Khoo <ak@Adrians-MacBook-Air.local>
2018-12-28 17:02:00 -0800

    Clicking on option changes CSS

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -47,9 +47,10 @@ const Option = styled.div`
   line-height: 19px;
   margin: 0;
   color: ${props => props.selected ? '#21ce99' : 'white'};
-  border-bottom: 2px solid ${props => props.selected ? '#21ce99' : 'white'};
+  border-bottom: 2px solid ${props => props.selected ? '#21ce99' : 'transparent'};
   padding-bottom: 12px;
-  margin: 0 12px
+  margin: 0 12px;
+  cursor: pointer;
 `;
 
 // ============================================
@@ -78,6 +79,7 @@ class App extends React.Component {
     this.setChangeCaption = this.setChangeCaption.bind(this);
     this.setDefaultChangeCaption = this.setDefaultChangeCaption.bind(this);
     this.handleMouseLeaveChart = this.handleMouseLeaveChart.bind(this);
+    this.handleOptionClick = this.handleOptionClick.bind(this);
   }
 
   setCompanyName(name) {
@@ -109,6 +111,14 @@ class App extends React.Component {
     this.setChangeCaption(this.state.defaultChangeCaption);
   }
 
+  handleOptionClick(e) {
+    if (e.target.classList.contains('option')) {
+      this.setState({
+        selectedGraph: e.target.innerHTML
+      });
+    }
+  }
+
   requestData(path, callback) {
     // Makes a get request to the provided path for a randomly generated stock
     axios.get('/stocks/')
@@ -136,13 +146,13 @@ class App extends React.Component {
         <div><Odometer id='price'value={this.state.selectedPrice} format='(,ddd).dd' duration={300}></Odometer></div>
         <div><Change id='change'>{change > 0 ? `+$${Math.abs(change)}` : `-$${Math.abs(change)}`} {'(' + changePercent + '%) '}</Change><ChangeCaption id='changeCaption'>{this.state.changeCaption}</ChangeCaption></div>
         <Chart5y requestData={this.requestData} setSelectedPrice={this.setSelectedPrice} handleMouseLeaveChart={this.handleMouseLeaveChart} setChangeCaption={this.setChangeCaption} setDefaultChangeCaption={this.setDefaultChangeCaption} tooltipY={tooltipY}/>
-        <Options>
-          <Option selected={this.state.selectedGraph === '1D'}>1D</Option>
-          <Option selected={this.state.selectedGraph === '1W'}>1W</Option>
-          <Option selected={this.state.selectedGraph === '1M'}>1M</Option>
-          <Option selected={this.state.selectedGraph === '3M'}>3M</Option>
-          <Option selected={this.state.selectedGraph === '1Y'}>1Y</Option>
-          <Option selected={this.state.selectedGraph === '5Y'}>5Y</Option>
+        <Options onClick={this.handleOptionClick}>
+          <Option className='option' selected={this.state.selectedGraph === '1D'}>1D</Option>
+          <Option className='option' selected={this.state.selectedGraph === '1W'}>1W</Option>
+          <Option className='option' selected={this.state.selectedGraph === '1M'}>1M</Option>
+          <Option className='option' selected={this.state.selectedGraph === '3M'}>3M</Option>
+          <Option className='option' selected={this.state.selectedGraph === '1Y'}>1Y</Option>
+          <Option className='option' selected={this.state.selectedGraph === '5Y'}>5Y</Option>
         </Options>
       </div>
     );
