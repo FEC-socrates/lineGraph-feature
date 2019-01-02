225cb9d18f8960a6455cf4055c11e20bc992bc4a
Adrian Khoo <adriankhoo.ca@gmail.com>
2019-01-02 15:47:09 -0800

    Removed unused imports.

--- client/components/chart.jsx
+++ client/components/chart.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 import Highcharts from 'highcharts';
 import axios from 'axios';
-import { callbackify } from 'util';
 
 class Chart extends React.Component {
   constructor(props) {
